d586205c648dce50fdb0dc277a31793415e7b55d
Marvin1261 <72426340+Marvin1261@users.noreply.github.com>
2022-10-23 23:36:22 -0600

    Add files via upload

---
+++ ConexionBD.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Modelo
+{
+    public class ConexionBD {
+        private string cadena = "server=localhost;database=db_empresa; user=root; password=villatoro@1261 ";
+        public MySqlConnection conectar = new MySqlConnection();
+
+        public void AbrirConexion(){
+            try
+            {
+                //conectar = new MySqlConnection();
+                conectar.ConnectionString = cadena;
+                conectar.Open();
+                //System.Diagnostics.Debug.WriteLine("Conexion Exitosa");
+            }
+            catch (Exception ex)
+            {
+                //System.Diagnostics.Debug.WriteLine(ex.Message);
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+        public void CerrarConexion(){
+            try
+            {
+                if(conectar.State == ConnectionState.Open)
+                {
+                    conectar.Close();
+                }
+                //System.Diagnostics.Debug.WriteLine("Cerrar Conexion");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+        }
+             
+    }
+}
---
+++ Empleado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Web.UI.WebControls;
+
+
+namespace Modelo
+{
+    public class Empleado{
+        ConexionBD conectar;
+    }
+}
